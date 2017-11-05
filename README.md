4206a4cb911743ca87c5d8a3f9f0327c25bbd43b
erictenenbaum <erictenenbaum10@gmail.com>
2017-11-05 09:01:33 -0800

    Almost finished with basic version. Would like to add more functionality, but just need to display previously guessed letters to complete very basic version

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # Hangman-Game
+
+Psuedo Code:
+
+1. Create the HTML Frame:
+	a. Wins
+	b. Losses
+	c. Remaining Guesses
+	d. Current Word
+
+2. Add <span> tags with unique id's to be changed when events occur
+
+3. Create an Array with all 26 letters.
+
+4. Create a variable for Wins, Losses, Remaining Guesses, and Current Word
+	a. Set Wins and Losses to 0
+
+5. Create and Array for the word bank
+
+6. Create a userGuess var = event.key
+
+
+
+
+11/5/2017 Left to do:
+
+1. Display letters that have been guessed.
+2. Possibly add hangman type picture.
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,63 +1,173 @@
 // Creates an array that lists out all of the options (Rock, Paper, or Scissors).
-    var computerChoices = ["r", "p", "s"];
-    var wins = 0;
-    var losses = 0;
-    var ties = 0;
+    console.log("yo")    
+   
 
-    // This function is run whenever the user presses a key.
-    document.onkeyup = function(event) {
+    //Have a randome word that when I press keys
+    //need to have some words we can chose from
+    var wordArray = ["baseball", "basketball", "football", "soccer", "hockey", "cricket"];
+    //need to chose one of those words at random
+    // console.log(Math.floor(Math.random() * wordArray.length));
 
-      // Determines which key was pressed.
-      var userGuess = event.key;
-      document.getElementById("userChoice").textContent = event.key;
+    var rando = Math.floor(Math.random() * wordArray.length);
+    console.log(wordArray[rando]);
+    //show blanks spaces to user to show how many letters  in word
 
-      // Randomly chooses a choice from the options array. This is the Computer's guess.
-      var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-      document.getElementById("computerChoice").textContent = computerGuess;
+    console.log(wordArray[rando].split(""));
+    var splitWordA = wordArray[rando].split("");
+    var chosenWUnderscore = [];
+    var remGuesses = 10;
+    var currentLosses = 0;
+    var currentWins = 0;
+    var gameScoreChange
+ 
 
-      // Alerts the key the user pressed (userGuess).
-      alert("User guess: " + userGuess);
+    for (var i = 0; i < splitWordA.length; i++) {
 
-      // Alerts the Computer's guess.
-      alert("Computer guess: " + computerGuess);
-
-      if ((userGuess === "r" || userGuess === "p" || userGuess === "s")) {
-        // if ((userGuess === "r") && (computerGuess === "s")) {
-        //   document.getElementById("userWins").innerHTML = wins++;
-        // }
-
-        // else if ((userGuess === "r") && (computerGuess === "r")) {
-        //   document.getElementById("userTies").innerHTML = ties++;
-        // }
-
-        // else if ((userGuess === "r") && (computerGuess === "p")) {
-        //   document.getElementById("userLoses").innerHTML = losses++;
-        // }
-
-        if (userGuess === computerGuess) {
-          document.getElementById("userTies").textContent = ++ties;
-          alert("You Tied");
-        }
-
-        else if ((userGuess === "r") && (computerGuess === "s")) {
-          document.getElementById("userWins").textContent = ++wins;
-          alert("You Win!");
-        }
-
-        else if ((userGuess === "p") && (computerGuess === "r")) {
-          document.getElementById("userWins").textContent = ++wins;
-          alert("You Win!");
-        }
-
-        else if ((userGuess === "s") && (computerGuess === "p")) {
-          document.getElementById("userWins").textContent = ++wins;
-          alert("You Win!");
-        }
-
-        else {
-          document.getElementById("userLoses").textContent = ++losses;
-          alert("You Lost");
-        }
-
-      }
+    	//gen underscore
+    	//push tthat array   	
+    	chosenWUnderscore.push("_")
     }
+
+    //show that array on screen
+	//display on screen so user can see its
+    document.getElementById("currentWord").innerHTML = chosenWUnderscore.join(" ");
+    
+    //see if this key i pressed exists in thsi word
+    //function that listens for key presses
+   document.addEventListener("keyup", function() {
+   	console.log(event);
+
+   	//user key they guessed
+   	console.log(event.key);
+   	var userGuess = event.key;
+   	for (var i = 0; i < splitWordA.length; i++) {
+
+   		if (userGuess == splitWordA[i]) {
+   			console.log("you found me");
+   			// console.log("this is what you are looking for: " + splitWordA[3]);
+   			chosenWUnderscore[i] = userGuess;  
+   			remGuesses = remGuesses + 1; 			
+   		}
+
+   		// else if (userGuess !== splitWordA[3]) {
+   		// 	console.log("not the one");
+   		// }
+   	}
+	
+	document.getElementById("currentWord").innerHTML = chosenWUnderscore.join(" ");	
+		
+
+		// if (userGuess !== splitWordA[i]) {
+		// 	// remGuesses = remGuesses - 1;
+		// 	console.log("not the one");
+
+		// }
+
+	// document.getElementById("remGuesses").innerHTML = remGuesses;		
+			
+		})
+   			document.addEventListener("keyup", function() {
+
+   					if (chosenWUnderscore.join(" ") == splitWordA.join(" ")) {
+					alert("You Win!")
+
+					var addWin = (currentWins = currentWins + 1);
+					// alert("You Lost!");
+					document.getElementById("winTotal").innerHTML = addWin;
+					var wordArray = ["baseball", "basketball", "football", "soccer", "hockey", "cricket"];
+					var rando = Math.floor(Math.random() * wordArray.length);
+				}					
+   			})	
+
+
+				   document.addEventListener("keyup", function() {
+
+				   		var userGuess = event.key;
+
+				   	 if (userGuess != splitWordA) {
+							var guessesLeft = (remGuesses = remGuesses - 1);
+							document.getElementById("remGuesses").innerHTML = guessesLeft;
+						};
+
+					if (remGuesses < 1) {
+
+						var addLoss = (currentLosses = currentLosses + 1);
+						alert("You Lost!");
+						document.getElementById("lossTotal").innerHTML = addLoss;
+						var wordArray = ["baseball", "basketball", "football", "soccer", "hockey", "cricket"];
+							   	var rando = Math.floor(Math.random() * wordArray.length);
+							   	var splitWordA = wordArray[rando].split("");
+							   	var chosenWUnderscore = [];
+							   	console.log(splitWordA);
+
+							   		for (var i = 0; i < splitWordA.length; i++) {
+							   			chosenWUnderscore.push("_")
+							   		}
+							   		document.getElementById("currentWord").innerHTML = chosenWUnderscore.join(" "); 
+							}			
+				   })
+
+
+   document.onclick = function reloadFunction() {
+   	location.reload();
+   }
+
+
+
+
+			
+
+   // })
+
+
+   // if its not in the word i want to see that it wasnt in the word
+   //I want to know if I can win or lose and see on the screen
+
+   
+
+
+
+
+
+
+
+
+
+
+
+  //  document.addEventListener("keyup", function() { 
+
+		// if (remGuesses < 1)  {
+		// 		var wordArray = ["baseball", "basketball", "football", "soccer", "hockey", "cricket"];
+		// 	   	var rando = Math.floor(Math.random() * wordArray.length);
+		// 	   	var splitWordA = wordArray[rando].split("");
+		// 	   	var chosenWUnderscore = [];
+		// 	   	console.log(splitWordA);
+
+		// 	   		for (var i = 0; i < splitWordA.length; i++) {
+		// 	   			chosenWUnderscore.push("_")
+		// 	   		}
+
+		// 	   		document.getElementById("currentWord").innerHTML = chosenWUnderscore.join(" ");  	
+    						
+    
+
+		// 	}
+
+		// })
+
+
+    
+
+    
+
+  
+
+
+
+
+
+
+
+
+
